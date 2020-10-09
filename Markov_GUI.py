161683b6b813d7c0e8516c42220cf86160932ffe
PhlegethonAcheron <53194290+PhlegethonAcheron@users.noreply.github.com>
2020-10-09 19:52:27 -0400

    Added a basic GUI, but didn't reformat the file to use proper object-oriented structure

---
+++ Markov_GUI.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+
+def gui_main():
+    markov_window = tk.Tk()
+    markov_window.title = "Markov Text Generator"
+    markov_window.mainloop()
+
+
+if __name__ == "__main__":
+    gui_main()
